23da56f3e2442ba5b5d203d6a07107dde630ccf8
Muneeb Aslam <muneeb.official.686332@hotmail.com>
2023-02-21 17:24:29 +0500

    Registration Doen

--- routes/login.js
+++ routes/login.js
@@ -1,25 +1,24 @@
-const bodyparser=require('body-parser')
-const router = require('express').Router()
-require("dotenv").config()
-const User = require('../model/user')
-const bcrypt = require('bcryptjs')
+const bodyparser = require("body-parser");
+const router = require("express").Router();
+require("dotenv").config();
+const User = require("../model/user");
+const bcrypt = require("bcryptjs");
 
+router.get("/", (req, res) => {
+    res.render("login");
+});
 
-router.get('/',(req,res)=>{
-    res.render('login')
-})
+router.get("/login", (req, res) => {
+    res.render("login");
+});
 
-router.get('/login',(req,res)=>{
-    res.render('login')
-})
+router.get("/forgot", (req, res) => {
+    res.render("forgot");
+});
 
-router.get('/forgot',(req,res)=>{
-    res.render('forgot')
-})
-
-router.get('/register',(req,res)=>{
-    res.render('register')
-})
+router.get("/register", (req, res) => {
+    res.render("register");
+});
 
 router.post("/login", async (req, res) => {
     const username = req.body.email;
@@ -44,35 +43,35 @@ router.post("/login", async (req, res) => {
     // } catch (err) {
     //     console.log(err);
     // }
-})
+});
 
-
-
-router.post('/register',async (req,res)=>{
-    const {name,email,password}= req.body; 
-    let errors = []
-    if(!name || !email || !password){
-        errors.push({msg:'Fill all the Fields'})
+router.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+    let errors = [];
+    if (!name || !email || !password) {
+        errors.push({ msg: "Fill all the Fields" });
     }
+    if (errors.length > 0)
+        res.render("register", { errors, name, email, password });
+    else {
+        User.findOne({ email: email }).then((user) => {
+            if (user) {
+                errors.push({ msg: "User Already Exists" });
+                res.render("register", { errors, name, email, password });
+            } else {
+                const newUser = new User({ name, email, password });
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save().then((user) => {
+                            res.redirect('login')
+                        });
+                    });
+                });
+            }
+        });
+    }
+});
 
-    if(errors.length>0)
-        res.render('register',{errors,name,email,password})
-    else
-        {
-            User.findOne({email:email}).then(user=>{
-                console.log(user);
-                if(user){
-                    errors.push("User Already Exists")
-                    console.log("H");
-                    res.render('register',{errors,name,email,password})
-                }else{
-                    const newUser = new User({name,email,password})
-                    
-                    res.send("Add User")
-                }
-            })
-
-        }
-})
-
-module.exports = router
+module.exports = router;
